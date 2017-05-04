8d23a5089d7b889824466412f2ea01a592789657
Roxk-33 <xiaorock@126.com>
2017-05-04 19:40:54 +0800

    2017.5.4 update InfiniteScroll

--- nuomi/MusicPlayer/js/music.js
+++ nuomi/MusicPlayer/js/music.js
@@ -2,21 +2,21 @@
  * Created by hao on 2017/3/30.
  */
 function readFile() {
-    var fso=new ActiveXObject(Scripting.FileSystemObject);
+    var fso = new ActiveXObject(Scripting.FileSystemObject);
 
 }
 function play_pause() {
     var myAudio = document.getElementById('myAudio'),
         $playBtn = $('#stop')
 
-    if($('#stop').is(':checked')){
+    if ($('#stop').is(':checked')) {
         console.log('stop');
-        $playBtn.attr("checked",false);
+        $playBtn.attr("checked", false);
         myAudio.play(); // 播放
 
-    }else {
+    } else {
         console.log('play')
-        $playBtn.attr('checked',true);
+        $playBtn.attr('checked', true);
         myAudio.pause(); // 暂停播放
     }
 }
@@ -25,13 +25,17 @@ function play_pause() {
  * @param {String} path 音乐路径
  * @constructor
  */
-function PlayMusic(path) {
+function PlayMusic() {
 
     // if(!path) return;
 
-    var $music = $("<audio autoplay='autoplay' id='myAudio'></audio>");
-    $music.attr("src","./music/OneRepublic - Apologize.mp3");
-    $('.container').append($music)
+
+    var Audio = document.createElement("audio");
+    Audio.id = "myAudio";
+    Audio.src = "./music/OneRepublic - Apologize.mp3";
+    Audio.autoplay = "autoplay";
+    Audio.volume = 0.5;
+    document.body.appendChild(Audio)
 }
 
 /**
@@ -43,17 +47,21 @@ var controlVoice = function () {
 
 controlVoice.prototype = {
 
-    init : function () {
+    init: function () {
         var oThis = this;
+        this.Audio = document.getElementById('myAudio');
+
+        this.getTime();
+
         $('.voice-btn').mousedown(function () {
-            oThis.moveVoice();
+            oThis.setVolume.call(oThis);
         })
         Event.add({
-            "Muted" : oThis.muted
+            "Muted": oThis.setMuted
         })
     },
 
-    moveVoice : function () {
+    setVolume: function () {
         var $voice = $('.voice-btn'),
             $voicepart = $(".voiceProcessNow"),
             oThis = this;
@@ -66,53 +74,109 @@ controlVoice.prototype = {
             left = left > 90 ? 90 : left;
             left = left < -5 ? -5 : left;
 
-            $voice.css("left",left + "px");
-            $voicepart.css("width",left+5 + "px")
+            $voice.css("left", left + "px");
+            $voicepart.css("width", left + 5 + "px")
 
             //控制音量
-            oThis.volume((left+5)/100);
+            oThis.Audio.volume = (left + 5) / 100;
+
+            return false;
         })
         $(document.body).mouseup(function () {
             $(document.body).unbind()
+            return false;
+
         })
     },
 
     //控制静音
-    muted : function () {
-        var  myAudio = document.getElementById('myAudio');
+    setMuted: function () {
+        var myAudio = document.getElementById('myAudio');
         myAudio.muted = !myAudio.muted;
 
         var $voice = $('.voice-btn'),
             $voicepart = $(".voiceProcessNow");
 
-        $voice.css("left",-5 + "px");
-        $voicepart.css("width",0 + "px")
+        $voice.css("left", -5 + "px");
+        $voicepart.css("width", 0 + "px")
+    },
+    setTime: function () {
+        var processAll = 500,
+            $process_btn = $(".process-btn");
+
+        $(".process-btn").mousedown(function (e) {
+            var event = window.event || e;
+
+        })
+
+    },
+    flashTime : function () {
+        var $process_btn = $(".process-btn"),
+            $processNow = $(".musicProcessNow"),
+            processAll = 500,
+            musicTime =  this.musciTime,
+            timer = null,
+            oThis = this;
+
+        // 秒/px
+        var rate = processAll/ musicTime;
+        console.log(rate)
+        timer = setInterval(function () {
+
+            console.log($processNow.width())
+            $process_btn.css("left", parseInt($process_btn.css("left")) + rate + "px");
+            $processNow.css("width",$processNow.width() + rate + "px");
+            oThis.Audio.ended && (clearInterval(timer));
+
+        },1000)
+
+    },
+    //获取总播放时长
+    getTime: function () {
+
+        var  oThis = this;
+
+        //获取总时长
+        this.Audio.addEventListener("loadedmetadata", function () {
+            oThis.musciTime = this.duration;
+            oThis.initPlayer.call(oThis,oThis.musciTime);
+        });
+
+        //格式化时间
+
     },
 
-    volume : function (curr_volume) {
-        var  myAudio = document.getElementById('myAudio');
+    //初始化播放器界面
+    initPlayer: function (data) {
 
-        myAudio.volume = curr_volume;
+        this.flashTime();
 
-    },
+        function format(data) {
 
-    currentTime : function (schedule) {
-        var  myAudio = document.getElementById('myAudio');
-        console.log(myAudio.duration)
-        if('fastSeek' in myAudio){
+            data = Math.floor(data);
 
-            myAudio.fastSeek(schedule);//改变audio.currentTime的值
+            var min = Math.floor(data / 100),
+                sec = data - min * 100;
 
+            sec = sec < 10 ? "0" + sec : sec;
+            min = min < 10 ? "0" + min : min;
+
+            return "-" + min + ":" + sec;
         }
-        myAudio.volume = schedule;
+
+        data = format(data);
+        var $showTime = $('.music-time');
+        console.log(data)
+        $showTime.html(data);
     }
 }
 
-window.onload = function(){
+window.onload = function () {
     PlayMusic();
     new controlVoice();
 
     Event.add({
-        "controlPlay" : play_pause
+        "controlPlay": play_pause
     })
+
 };
---
+++ shangye/InfiniteScroll/InfiniteScroll.js
@@ -0,0 +1,75 @@
+/**
+ * Created by hao on 2017/5/4.
+ */
+(function ($) {
+
+    $.fn.infiniteScroll = function (option) {
+        var
+            setting = $.extend({
+                liClassName : "",
+                divClassName : "",
+                iniNum : 28 ,
+                addNum : 10 ,
+                waitLoadding : 2000,
+            },option),
+            $view = $(this).find("ul"),
+            $theDocument = $(document),
+            $theWindow = $(window),
+            numList = 0,
+            appendList = function (num) {
+                for(var i = 0; i < num; i++){
+                    numList++;
+                    var
+                        $moduleLi = $("<li></li>").addClass(setting.liClassName),
+                        $moduleDiv = $("<div></div>").addClass(setting.divClassName);
+
+                    $moduleDiv.text("Item " + numList);
+                    $moduleLi.append($moduleDiv);
+                    $view.append($moduleLi);
+                }
+            },
+            iniList = function () {
+                appendList(setting.iniNum);
+                $theWindow.scroll(function () {
+                    scrollAppend();
+                });
+            };
+
+        function animationLoadding() {
+            // var oDiv = $("<div></div>").css({
+            //     "width": "40px",
+            //     "height": "40px",
+            //     "margin": "10px auto",
+            //     "border-radius": "50%",
+            //     "border": "5px solid #878787",
+            //     "border-left-color": "transparent",
+            //     "display" : "none"
+            // })
+            // oDiv.animate()
+
+            $(".loadding").css("display", "block");
+            setTimeout(function () {
+                $(".loadding").css("display", "none");
+            },setting.waitLoadding)
+        }
+        function scrollAppend() {
+
+            var height = $theDocument.height();
+            if($theWindow.height() + $theWindow.scrollTop() < height) return;
+            setTimeout(function () {
+                appendList(setting.addNum)
+            },setting.waitLoadding)
+            animationLoadding();
+
+        }
+
+        iniList();
+        return this;
+    }
+
+
+
+
+
+
+}(jQuery))
